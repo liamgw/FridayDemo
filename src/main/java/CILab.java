ce90deb6b358498060c242fb1c4c01c47620587b
Liam G-Watson <liamgriskey@gmail.com>
2021-06-12 23:02:59 -0700

    Implemented dectectCapitalUse method logic

--- src/main/java/CILab.java
+++ src/main/java/CILab.java
@@ -13,6 +13,20 @@ public class CILab implements CILabInterface {
 
     @Override
     public boolean detectCapitalUse() {
+        String exampleWord = getString();
+
+        int capsCount = 0;
+        for (int i = 0; i < exampleWord.length(); i++){
+            if (Character.isUpperCase(exampleWord.charAt(i))) {
+                capsCount++;
+            }
+        }
+
+        if(capsCount == exampleWord.length() || capsCount == 0)
+            return true;
+        if (capsCount == 1 && Character.isUpperCase(exampleWord.charAt(0)))
+            return true;
+
         return false;
     }
 
