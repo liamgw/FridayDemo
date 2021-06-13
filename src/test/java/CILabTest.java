8ff86dec79474d6c6737e4cf561c49e0c59bcaf4
Liam G-Watson <liamgriskey@gmail.com>
2021-06-12 23:23:20 -0700

    Added test cases, all now pass

--- src/test/java/CILabTest.java
+++ src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CILabTest {
 
@@ -20,12 +20,38 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-       fail("Not yet implemented");
-
+       myString.setString("Mongolia");
+       assertTrue(myString.detectCapitalUse());
     }
+
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("FRANCE");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("belarus");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("paragUay");
+        assertFalse(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest5() {
+        myString.setString("13ra zi1");
+        assertTrue(myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest6() {
+        myString.setString("GermanY");
+        assertFalse(myString.detectCapitalUse());
     }
 
 
